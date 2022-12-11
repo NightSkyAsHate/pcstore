f70fc3d53090a8c79d276af53ce3247f98ab94f0
German Kazakov <german.kazakov@simbirsoft.com>
2022-12-11 22:29:39 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   Header
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+	font-family: 'Lato', sans-serif;
+	font-weight: 400;
+	font-size: 16px;
+	line-height: 1.7;
+	color: #eee;
 }
 </style>
---
+++ src/components/atoms/Logo.vue
@@ -0,0 +1,11 @@
+<template>
+  <a href="/" class="w-[8rem] hover:opacity-70">
+    <img src="../../assets/img/logo.jpg" alt="logo" />
+  </a>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'Logo'
+}
+</script>
